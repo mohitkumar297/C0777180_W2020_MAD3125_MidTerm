e5bfca51e44aac94cce9bf0a028edd97560ed76a
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-08 12:08:38 -0400

    [MK] Added code for age calculation

--- app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
@@ -1,6 +1,8 @@
 package com.example.c0777180_w2020_mad3125_midterm;
 
 import android.app.DatePickerDialog;
+import android.icu.util.LocaleData;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,11 +10,14 @@ import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.time.LocalDate;
 import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -44,6 +49,7 @@ public class PersonInformationActivity extends AppCompatActivity {
     int month;
     int dayOfMonth;
     Calendar calendar;
+    int age = 0 ;
 
     DatePickerDialog datePickerDialog;
     @Override
@@ -53,6 +59,7 @@ public class PersonInformationActivity extends AppCompatActivity {
         ButterKnife.inject(this);
 
         btnDOB.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
 
@@ -60,15 +67,19 @@ public class PersonInformationActivity extends AppCompatActivity {
                 year= calendar.get(Calendar.YEAR);
                 month = calendar.get(Calendar.MONTH);
                 dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                final LocalDate today = LocalDate.now();
                 datePickerDialog = new DatePickerDialog(PersonInformationActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        btnDOB.setText(dayOfMonth + "/" + month + "/" + year);
+                        btnDOB.setText(dayOfMonth + "-" + month + "-" + year);
+
+                        age = today.getYear() - year;
                     }
 
                 }, year,month,dayOfMonth );
                 datePickerDialog.show();
             }
         }) ;
+        
     }
 }
