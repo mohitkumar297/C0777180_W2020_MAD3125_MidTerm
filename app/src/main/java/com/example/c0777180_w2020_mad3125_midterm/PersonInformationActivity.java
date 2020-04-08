71ce3a0ecbcc74763dea3529fade90e0f24cd73c
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-08 11:51:40 -0400

    [MK] Added injections

--- app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
@@ -1,12 +1,39 @@
 package com.example.c0777180_w2020_mad3125_midterm;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class PersonInformationActivity extends AppCompatActivity {
+
+    @InjectView(R.id.txtSIN)
+    TextInputEditText txtSIN;
+    @InjectView(R.id.txtFirstName)
+    TextInputEditText txtFirstName;
+    @InjectView(R.id.txtLastName)
+    TextInputEditText txtLastName;
+    @InjectView(R.id.btnDOB)
+    Button btnDOB;
+    @InjectView(R.id.radioMale)
+    RadioButton radioMale;
+    @InjectView(R.id.radioFemale)
+    RadioButton radioFemale;
+    @InjectView(R.id.radioOther)
+    RadioButton radioOther;
+    @InjectView(R.id.radio)
+    RadioGroup radio;
+    @InjectView(R.id.txtGrossIncome)
+    TextInputEditText txtGrossIncome;
+    @InjectView(R.id.txtRRSPContributed)
+    TextInputEditText txtRRSPContributed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
