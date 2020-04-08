433c4e223a35672b36d211c6dc7504ca4eff9489
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-08 11:49:05 -0400

    [MK] Added butterknife injection line

--- app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class PersonInformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_information);
-
+        ButterKnife.inject(this);
     }
 }
