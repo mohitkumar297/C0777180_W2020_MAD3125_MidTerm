a4878cc820fad75db4477a07a303ee3bc65a8518
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-08 01:07:29 -0400

    [MK]Initial commit

---
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
@@ -0,0 +1,15 @@
+package com.example.c0777180_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_information);
+
+    }
+}
---
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.c0777180_w2020_mad3125_midterm;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, PersonInformationActivity.class);
+                startActivity(homeIntent);
+
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
+
