6b8be775c57ce48d236a5ee92c0186d527702bed
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-08 13:06:02 -0400

    [MK] Generated the butterknife

--- app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformationActivity.java
@@ -1,23 +1,19 @@
 package com.example.c0777180_w2020_mad3125_midterm;
 
 import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -54,6 +50,8 @@ public class PersonInformationActivity extends AppCompatActivity {
     DatePickerDialog datePickerDialog;
     @InjectView(R.id.txtTaxFilingDate)
     TextView txtTaxFilingDate;
+    @InjectView(R.id.txtDOB)
+    EditText txtDOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +63,7 @@ public class PersonInformationActivity extends AppCompatActivity {
         String today = sdf.format(new Date());
         txtTaxFilingDate.setText(today);
 
-        btnDOB.setOnClickListener(new View.OnClickListener() {
+        txtDOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -73,7 +71,7 @@ public class PersonInformationActivity extends AppCompatActivity {
                 year = calendar.get(Calendar.YEAR);
                 month = calendar.get(Calendar.MONTH);
                 dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-                datePickerDialog = new DatePickerDialog(PersonInformationActivity.this,datePickerListener, year, month, dayOfMonth);
+                datePickerDialog = new DatePickerDialog(PersonInformationActivity.this, datePickerListener, year, month, dayOfMonth);
                 datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
                 datePickerDialog.show();
             }
@@ -88,21 +86,21 @@ public class PersonInformationActivity extends AppCompatActivity {
             c.set(Calendar.MONTH, month);
             c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
             String format = new SimpleDateFormat("dd-MM-YYYY").format(c.getTime());
-            btnDOB.setText(format);
-           //FOR AGE DISPLAY txtTaxFilingDate.setText(Integer.toString(calculateAge(c.getTimeInMillis())));
+            txtDOB.setText(format);
+            //FOR AGE DISPLAY txtTaxFilingDate.setText(Integer.toString(calculateAge(c.getTimeInMillis())));
         }
 
 
     };
 
-    public int calculateAge(long date){
-       Calendar dob = Calendar.getInstance();
-       dob.setTimeInMillis(date);
+    public int calculateAge(long date) {
+        Calendar dob = Calendar.getInstance();
+        dob.setTimeInMillis(date);
 
-       Calendar today = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
 
-       age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
-       return age;
+        age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        return age;
 
     }
 }
