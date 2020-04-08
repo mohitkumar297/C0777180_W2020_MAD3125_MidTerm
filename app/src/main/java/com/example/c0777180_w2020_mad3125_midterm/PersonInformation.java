ec6f470b42a4220332716772572c3a6a44ba212d
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-08 01:24:48 -0400

    [MK] Added getter and setter to PersonInformation

--- app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformation.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/PersonInformation.java
@@ -22,6 +22,145 @@ public class PersonInformation implements Parcelable {
     private float totalTaxableIncome;
     private float totalTaxPayed;
 
+    public String getSin() {
+        return sin;
+    }
+
+    public void setSin(String sin) {
+        this.sin = sin;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public float getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(float grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public float getRRSPContributed() {
+        return RRSPContributed;
+    }
+
+    public void setRRSPContributed(float RRSPContributed) {
+        this.RRSPContributed = RRSPContributed;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getTaxFilingDate() {
+        return taxFilingDate;
+    }
+
+    public void setTaxFilingDate(String taxFilingDate) {
+        this.taxFilingDate = taxFilingDate;
+    }
+
+    public float getFederalTax() {
+        return federalTax;
+    }
+
+    public void setFederalTax(float federalTax) {
+        this.federalTax = federalTax;
+    }
+
+    public float getProvincialTax() {
+        return provincialTax;
+    }
+
+    public void setProvincialTax(float provincialTax) {
+        this.provincialTax = provincialTax;
+    }
+
+    public float getCPP() {
+        return CPP;
+    }
+
+    public void setCPP(float CPP) {
+        this.CPP = CPP;
+    }
+
+    public float getEI() {
+        return EI;
+    }
+
+    public void setEI(float EI) {
+        this.EI = EI;
+    }
+
+    public float getCarryForwardRRSP() {
+        return carryForwardRRSP;
+    }
+
+    public void setCarryForwardRRSP(float carryForwardRRSP) {
+        this.carryForwardRRSP = carryForwardRRSP;
+    }
+
+    public float getTotalTaxableIncome() {
+        return totalTaxableIncome;
+    }
+
+    public void setTotalTaxableIncome(float totalTaxableIncome) {
+        this.totalTaxableIncome = totalTaxableIncome;
+    }
+
+    public float getTotalTaxPayed() {
+        return totalTaxPayed;
+    }
+
+    public void setTotalTaxPayed(float totalTaxPayed) {
+        this.totalTaxPayed = totalTaxPayed;
+    }
+
+    public static Creator<PersonInformation> getCREATOR() {
+        return CREATOR;
+    }
 
     protected PersonInformation(Parcel in) {
     }
