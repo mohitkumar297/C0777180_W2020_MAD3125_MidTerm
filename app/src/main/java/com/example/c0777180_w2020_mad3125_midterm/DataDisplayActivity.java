24bdbe0d36e90799f84bff45d061c2dc0795947b
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-08 20:20:52 -0400

    [MK] Added butterknife injection to get IDs

--- app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/DataDisplayActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/DataDisplayActivity.java
@@ -1,12 +1,29 @@
 package com.example.c0777180_w2020_mad3125_midterm;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class DataDisplayActivity extends AppCompatActivity {
+
+    @InjectView(R.id.SIN)
+    TextView SIN;
+    @InjectView(R.id.age)
+    TextView age;
+    @InjectView(R.id.fullname)
+    TextView fullname;
+    @InjectView(R.id.gender)
+    TextView gender;
+    @InjectView(R.id.taxdate)
+    TextView taxdate;
+    @InjectView(R.id.grossincome)
+    TextView grossincome;
+    @InjectView(R.id.rrspcontributed)
+    TextView rrspcontributed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
