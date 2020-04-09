e74707798d8beca11d23d8826148ae96498cf5f3
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-09 18:55:07 -0400

    [MK] Changed splash screen

--- app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/SplashActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_midterm/SplashActivity.java
@@ -5,8 +5,12 @@ package com.example.c0777180_w2020_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -14,15 +18,22 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(SplashActivity.this, PersonInformationActivity.class);
-                startActivity(homeIntent);
+        EasySplashScreen config = new EasySplashScreen(SplashActivity.this)
+                .withFullScreen()
+                .withTargetActivity((PersonInformationActivity.class))
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#1a1b29"))
+                .withAfterLogoText("\n\nMohit Kumar\n\n  C0777180")
+                .withLogo(R.drawable.dollar);
 
-            }
-        },SPLASH_TIME_OUT);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextSize(20);
+
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+
+
     }
 }
 
